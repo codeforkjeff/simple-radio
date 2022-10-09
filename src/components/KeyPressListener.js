9ad39c4013814825277fb7fc0796c1316729b791
codeforkjeff <jeff@codefork.com>
2022-10-08 23:52:19 -0700

    don't respond to key events in inputs

--- src/components/KeyPressListener.js
+++ src/components/KeyPressListener.js
@@ -10,17 +10,19 @@ const KeyPressListener = ({ playerState: { streams, currentStreamIndex }, dispat
       console.log("adding keypress listener")
       window.addEventListener('keydown', e => {
         console.log(e.key)
-        if("123456789".indexOf(e.key) !== -1) {
-          const index = parseInt(e.key) - 1
-          if(index < streams.length) {
-            dispatchPlayer({ type: "play_stream", currentStreamIndex: index })
+        if(e.target.tagName !== 'INPUT') {
+          if("123456789".indexOf(e.key) !== -1) {
+            const index = parseInt(e.key) - 1
+            if(index < streams.length) {
+              dispatchPlayer({ type: "play_stream", currentStreamIndex: index })
+            }
           }
-        }
-        else if (e.key === 'ArrowRight') {
-          dispatchPlayer({ type: "skip_forward" })
-        }
-        else if (e.key === 'ArrowLeft') {
-          dispatchPlayer({ type: "skip_backward" })
+          else if (e.key === 'ArrowRight') {
+            dispatchPlayer({ type: "skip_forward" })
+          }
+          else if (e.key === 'ArrowLeft') {
+            dispatchPlayer({ type: "skip_backward" })
+          }
         }
       })
     }
