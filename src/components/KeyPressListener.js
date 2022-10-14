b80848cbbec7541e9bcc3c862fdc53ee85357cf4
codeforkjeff <jeff@codefork.com>
2022-10-13 22:51:15 -0700

    make keypress listener work again

--- src/components/KeyPressListener.js
+++ src/components/KeyPressListener.js
@@ -2,30 +2,27 @@ import { useEffect, useRef } from 'react';
 
 const KeyPressListener = ({ playerState: { streams, currentStreamIndex }, dispatchPlayer }) => {
 
-  // ensures that useEffect below runs only once even in strict mode
-  const runOnce = useRef(false)
-
   useEffect(() => {
-    if(!runOnce.current) {
-      window.addEventListener('keydown', e => {
-        //console.log(e.key)
-        if(e.target.tagName !== 'INPUT') {
-          if("123456789".indexOf(e.key) !== -1) {
-            const index = parseInt(e.key) - 1
-            if(index < streams.length) {
-              dispatchPlayer({ type: "play_stream", currentStreamIndex: index })
-            }
-          }
-          else if (e.key === 'ArrowRight') {
-            dispatchPlayer({ type: "skip_forward" })
-          }
-          else if (e.key === 'ArrowLeft') {
-            dispatchPlayer({ type: "skip_backward" })
+    const listener = (e) => {
+      if(e.target.tagName !== 'INPUT') {
+        if("123456789".indexOf(e.key) !== -1) {
+          const index = parseInt(e.key) - 1
+          if(index < streams.length) {
+            dispatchPlayer({ type: "play_stream", currentStreamIndex: index })
           }
         }
-      })
+        else if (e.key === 'ArrowRight') {
+          dispatchPlayer({ type: "skip_forward" })
+        }
+        else if (e.key === 'ArrowLeft') {
+          dispatchPlayer({ type: "skip_backward" })
+        }
+      }
     }
-    runOnce.current = true
+    window.addEventListener('keydown', listener)
+    return () => {
+      window.removeEventListener('keydown', listener)
+    }
   })
 }
 
