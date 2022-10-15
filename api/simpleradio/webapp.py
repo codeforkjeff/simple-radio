ab1b49aead2bc463ff2e2870d78b2132f52e5269
codeforkjeff <jeff@codefork.com>
2022-10-15 13:35:53 -0700

    load default stream from config table

--- api/simpleradio/webapp.py
+++ api/simpleradio/webapp.py
@@ -1,10 +1,12 @@
 
 from fastapi import APIRouter, FastAPI
 from . import hashed_content
+from . import config
 
 v1 = FastAPI()
 
 v1.include_router(hashed_content.router)
+v1.include_router(config.router)
 
 app = FastAPI()
 
--- src/components/AppRoot.js
+++ src/components/AppRoot.js
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
 function AppRoot() {
 
   console.log("rendering AppRoot")
 
+  const [ streamListId, setStreamListId ] = useState(null)
+  const [ error, setError ] = useState(null)
+
+  useEffect(() => {
+    fetch(`/api/v1/config/`)
+    .then(res => res.json())
+    .then((result) => {
+      setStreamListId(result.config.default_stream_list_id)
+    })
+    .catch((error) => {
+      setError("Error loading config: " + error)
+    })
+  })
+
   return (
-    <Navigate to="/streamlist/3939c14eb6" />
+    <>
+    {streamListId && (
+      <Navigate to={`/streamlist/${streamListId}`} />
+    )}
+    {error && (
+      <p>Couldn't load application configuration, backend is probably not running</p>
+    )}
+    </>
   )
 }
 
